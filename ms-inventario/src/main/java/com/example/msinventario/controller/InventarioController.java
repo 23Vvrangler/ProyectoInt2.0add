a3ed38d48d7b444a6ffab502ee4c16bc723f6510
DarkingNro10 <marcosfelipevaleriano17@gmail.com>
2024-06-03 08:59:31 -0500

    dos

--- ms-inventario/src/main/java/com/example/msinventario/controller/InventarioController.java
+++ ms-inventario/src/main/java/com/example/msinventario/controller/InventarioController.java
@@ -2,6 +2,7 @@ package com.example.msinventario.controller;
 
 import com.example.msinventario.entity.Inventario;
 import com.example.msinventario.service.InventarioService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +12,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/inventario")
 public class InventarioController {
+
     @Autowired
     private InventarioService inventarioService;
+
+    // Implementación del circuit breaker para el método buscarPorId
+    @GetMapping("/{id}")
+    @CircuitBreaker(name = "buscarPorIdCB", fallbackMethod = "buscarPorIdFallback")
+    public ResponseEntity<Inventario> buscarPorId(@PathVariable Long id) {
+        Inventario inventario = inventarioService.buscarPorId(id);
+        if (inventario != null) {
+            return ResponseEntity.ok(inventario);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Método de fallback para buscarPorId en caso de que el circuit breaker esté abierto o haya un error
+    public ResponseEntity<Inventario> buscarPorIdFallback(Long id, Throwable throwable) {
+        // Manejo del error o respuesta predeterminada
+        return ResponseEntity.notFound().build(); // Por ejemplo, devolver una respuesta not found
+    }
 
     @GetMapping
     public ResponseEntity<List<Inventario>> listar() {
@@ -22,16 +42,6 @@ public class InventarioController {
     @PostMapping
     public ResponseEntity<Inventario> guardar(@RequestBody Inventario inventario) {
         return ResponseEntity.ok(inventarioService.guardar(inventario));
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Inventario> buscarPorId(@PathVariable Long id) {
-        Inventario inventario = inventarioService.buscarPorId(id);
-        if (inventario != null) {
-            return ResponseEntity.ok(inventario);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
     }
 
     @GetMapping("/producto/{productoId}")
